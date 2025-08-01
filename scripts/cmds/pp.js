1d28e8a160e8a3a9349ebf605a4f6581f05f7035
moronali09 <jonibou04@gmail.com>
2025-08-02 02:44:54 +0600

    Update pp.js

--- scripts/cmds/pp.js
+++ scripts/cmds/pp.js
@@ -1,79 +1,38 @@
-const fs = global.nodemodule['fs'] || require('fs');
-const request = global.nodemodule['request'] || require('request');
-const axios = global.nodemodule['axios'] || require('axios');
-const path = require('path');
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
-module.exports.config = {
-  name: "pp",
-  version: "1.0.5",
-  hasPermssion: 0,
-  credits: "moronali",
-  description: "📸 Fetch Facebook profile picture",
-  commandCategory: "tools",
-  cooldowns: 5,
-  usages: "{pn} [id|mention|profileURL]"
-};
+module.exports = {
+  config: {
+    name: "pp",
+    version: "1.0",
+    author: "moron ali",
+    countDown: 5,
+    role: 0,
+    shortDescription: "Profile picture",
+    longDescription: "Fetch profile picture of a user",
+    category: "media",
+    guide: "{p}pp (mention/reply/nothing)"
+  },
 
-module.exports.onStart = async function({ event, api, args, Users }) {
-  try {
-    let uid;
+  onStart: async function ({ message, event, usersData }) {
+    const targetID = event.type === "message_reply"
+      ? event.messageReply.senderID
+      : (event.mentions && Object.keys(event.mentions)[0]) || event.senderID;
 
-    if (event.type === 'message_reply' && event.messageReply) {
-      uid = event.messageReply.senderID;
-    }
+    const url = `https://graph.facebook.com/${targetID}/picture?width=720&height=720&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32`;
 
-    else if (event.mentions && Object.keys(event.mentions).length > 0) {
-      uid = Object.keys(event.mentions)[0];
-    }
-
-    else if (args[0] && args[0].includes('facebook.com/')) {
-      uid = await api.getUID(args[0]);
-    }
-
-    else if (args[0]) {
-      uid = args[0];
-    }
-
-    else {
-      uid = event.senderID;
-    }
-
-
-    const cacheDir = path.join(__dirname, 'cache');
+    // Make sure cache folder exists
+    const cacheDir = path.join(__dirname, "cache");
     if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
 
+    const filePath = path.join(cacheDir, `${targetID}.jpg`);
+    const img = (await axios.get(url, { responseType: "arraybuffer" })).data;
+    fs.writeFileSync(filePath, Buffer.from(img, "utf-8"));
 
-    const accessToken = '6628568379|c1e620fa708a1d5696fb991c1bde5662';
-    const picUrl = `https://graph.facebook.com/${uid}/picture?width=1500&height=1500&access_token=${accessToken}`;
-    const imagePath = path.join(cacheDir, 'pp.png');
-
-
-    await new Promise((resolve, reject) => {
-      request(encodeURI(picUrl))
-        .pipe(fs.createWriteStream(imagePath))
-        .on('close', resolve)
-        .on('error', reject);
+    const name = await usersData.getName(targetID);
+    message.reply({ body: ` `, attachment: fs.createReadStream(filePath) }, () => {
+      fs.unlinkSync(filePath);
     });
-
-
-    let name;
-    try {
-      name = await Users.getNameUser(uid);
-    } catch {
-      name = uid;
-    }
-
-
-    await api.sendMessage(
-      { body: `  -==Profile==`, attachment: fs.createReadStream(imagePath) },
-      event.threadID,
-      () => fs.unlinkSync(imagePath),
-      event.messageID
-    );
-
-  } catch (err) {
-    console.error('PP Error:', err);
-    return api.sendMessage(`❌ Lỗi: ${err.message}`, event.threadID, event.messageID);
   }
 };
-             
