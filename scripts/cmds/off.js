2ce544df8ecf72fc6145adbfb59273a0e86b16f8
moronali09 <jonibou04@gmail.com>
2025-08-03 14:54:39 +0600

    Update off.js

--- scripts/cmds/off.js
+++ scripts/cmds/off.js
@@ -1,18 +1,31 @@
 module.exports = {
-    config: {
-        name: "off",
-        version: "1.0",
-        author: "MahMUD",
-        countDown: 0,
-        role: 2,
-        category: "utility",
-        guide: "{p}off"
-    },
+  config: {
+    name: "off",
+    version: "1.2",
+    author: "MahMUD",
+    countDown: 0,
+    role: 0,
+    category: "utility",
+    guide: "{p}off confirm",
 
-    onStart: async function ({ api, event, message }) {
 
-        await message.reply("Bot is shutting down...");
+    
+    ownerID: "100046458343946"
+  },
 
-        process.exit(0);
+  onStart: async function({ api, event, args, message }) {
+    const senderID = event.senderID;
+    const { ownerID } = module.exports.config;
+
+    if (senderID !== ownerID) {
+      return message.reply("⛔ You don't have permission to use this command.");
     }
+
+    if (!args[0] || args[0].toLowerCase() !== "confirm") {
+      return message.reply("🤖 Are you sure you want to shut me down?\nIf yes, type: {p}off confirm");
+    }
+
+    await message.reply("💤 Shutting down... Goodbye!");
+    process.exit(0);
+  }
 };
