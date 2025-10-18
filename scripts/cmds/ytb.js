6b0271736a574a048d94c80cb60ad5e0551560ff
moronali09 <jonibou04@gmail.com>
2025-10-18 21:26:45 +0600

    Update ytb.js

--- scripts/cmds/ytb.js
+++ scripts/cmds/ytb.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const baseApiUrl = async () => {
 	const base = await axios.get(
-`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+` `
 	);
 	return base.data.api;
 };
